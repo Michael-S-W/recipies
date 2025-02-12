527b20bf7d89d6b98848dc6289583c0479a9367f
Michael <michael_s.wilson@ymail.com>
2025-02-12 16:32:11 +0200

    updating routes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/recipies' element={<Home/>}/>
       <Route path=':country' element={<Country/>}/>
       <Route path=':country/:mealId' element={<Meal/>}/>
       <Route path='*' element={<Unknown/>}/>
